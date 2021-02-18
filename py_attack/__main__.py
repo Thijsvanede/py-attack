553cae6857f2e5e6c188c6bb30a2b6695b51f840
Thijs van Ede <t.s.vanede@utwente.nl>
2021-02-18 15:36:01 +0100

    New main method, query related concepts

--- py_attack/__main__.py
+++ py_attack/__main__.py
@@ -1,67 +1,13 @@
 from py_attack import ATTACK
-import networkx as nx
 
 if __name__ == "__main__":
-    from time import time
+    # Download latest ATTACK framework
+    attack = ATTACK.download()
 
-    start = time()
-    attack = ATTACK.load(
-        path    = '/home/thijs/Documents/research/eagle/data/cti/{domain}-attack/{domain}-attack.json',
-        domains = ['enterprise', 'mobile', 'ics', 'pre'],
-    )
-    print("Loading took {} seconds".format(time() - start))
-
-    start = time()
-    attack.store_pickle('models/attack.p')
-    print("Pickling took {} seconds".format(time() - start))
-    start = time()
-    attack = ATTACK.load_pickle('models/attack.p')
-    print("Unpickling took {} seconds".format(time() - start))
-
-    graph = attack.graph
-    neighbors = attack.related_concepts('T1087', depth=1)
-    for n in sorted(neighbors):
-        print(n)
-    exit()
-
-    # # Create ATT&CK framework
-    # start = time()
-    # attack = ATTACK.load("../data/cti")
-    # print("Load took {} seconds".format(time() - start))
     #
-    # start = time()
-    # # Retrieve objects from framework
-    # matrices       = attack.matrices
-    # tactics        = attack.tactics
-    # techniques     = attack.techniques
-    # sub_techniques = attack.sub_techniques
-    # procedures     = attack.procedures
-    # mitigations    = attack.mitigations
-    # groups         = attack.groups
-    # software       = attack.software
-    # print("Indexing took {} seconds".format(time() - start))
-    #
-    # start = time()
-    # attack.store_pickle('attack.p')
-    # print("Store took {} seconds".format(time() - start))
-    start = time()
-    attack = ATTACK.load_pickle('/home/thijs/Documents/research/eagle/py-attack/models/attack.p')
-    attack.clear()
-    print("Load_pickle took {} seconds".format(time() - start))
+    while True:
+        concept = input("Find related concepts for ID (e.g., T1087): ")
 
-    print(attack.summary())
-    start = time()
-    matrices       = attack.matrices
-    tactics        = attack.tactics
-    techniques     = attack.techniques
-    sub_techniques = attack.sub_techniques
-    procedures     = attack.procedures
-    mitigations    = attack.mitigations
-    groups         = attack.groups
-    software       = attack.software
-    print("Indexing took {} seconds".format(time() - start))
-
-    graph = attack.graph
-    print(len(graph.nodes()))
-    print(len(graph.edges()))
-    nx.write_gexf(graph, 'models/graph.gexf')
+        for related in sorted(attack.related_concepts(concept, depth=1)):
+            print('  {}'.format(related))
+        print()
