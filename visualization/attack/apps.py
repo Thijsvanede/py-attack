22bc22e38b1adfbc3d350872b7cdd4b1cc0cefc2
Thijs van Ede <t.s.vanede@utwente.nl>
2022-11-28 11:24:06 +0100

    Removed visualization test

--- visualization/attack/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AttackConfig(AppConfig):
-    name = 'attack'
--- visualization/attack/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from py_attack.attack import ATTACK
-
-def index(request):
-    # Set empty context
-    context = dict()
-
-    # Return webpage
-    return render(request, "attack/index.html", context)
